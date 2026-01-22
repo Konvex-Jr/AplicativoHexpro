ad95a89529107e2b1b6d6305b473ac9d62febc59
artPerozin <perozin.arthur@gmail.com>
2026-01-22 19:00:58 -0300

    arruma arquivos de configuração para build

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,12 @@
-import { useState } from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { WebView } from "react-native-webview";
 
-export default function Home() {
-  const [loading, setLoading] = useState(true);
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      {loading && (
-        <ActivityIndicator size="large" style={styles.loading} />
-      )}
-
       <WebView
         source={{ uri: "https://hexpro.com.br/" }}
-        onLoadEnd={() => setLoading(false)}
         javaScriptEnabled
         domStorageEnabled
+        startInLoadingState
       />
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-  loading: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginLeft: -20,
-    marginTop: -20,
-    zIndex: 10,
-  },
-});
